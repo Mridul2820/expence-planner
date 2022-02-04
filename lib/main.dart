230b8e0d2032800e7d9081784a9ee5f3f0ff8b19
Mridul2820 <immridulpanda2820@gmail.com>
2022-02-04 21:24:00 +0530

    widgit breakup

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,9 @@
-import './transaction.dart';
+import './widgets/user_transactions.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-      id: 't1',
-      title: 'Book',
-      amount: 500.00,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Shoe',
-      amount: 1500.00,
-      date: DateTime.now(),
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -33,61 +17,11 @@ class MyApp extends StatelessWidget {
             Card(
               child: Container(
                 width: double.infinity,
-                child: Text('Card'),
+                child: Text('CHARTS'),
               ),
               elevation: 5,
             ),
-            Column(
-              children: transactions
-                  .map(
-                    (tx) => Card(
-                      child: Row(
-                        children: [
-                          Container(
-                            margin: EdgeInsets.symmetric(
-                              vertical: 10,
-                              horizontal: 15,
-                            ),
-                            padding: EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Colors.purple,
-                                width: 2,
-                              ),
-                            ),
-                            child: Text(
-                              '\u{20B9}${tx.amount}',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 20,
-                                color: Colors.purple,
-                              ),
-                            ),
-                          ),
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                tx.title,
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Text(
-                                DateFormat.yMd().add_jm().format(tx.date),
-                                style: TextStyle(
-                                  color: Colors.grey,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
-                  .toList(),
-            ),
+            UserTransaction(),
           ],
         ),
       ),
